c442e63881babc10d49092614971366a0dd188d5
jsubiasm <jsubiasm@yahoo.es>
2018-10-05 09:37:08 +0200

    Modificadas urls servicios para apuntar a todoappv2-zxque

--- src/providers/todo-service/todo-service.ts
+++ src/providers/todo-service/todo-service.ts
@@ -14,33 +14,33 @@ export class TodoServiceProvider {
   }
 
   getNotas() {
-    return this.http.get('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoapp-rufkc/service/TODOService/incoming_webhook/getNotas');
+    return this.http.get('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoappv2-zxque/service/TODOService/incoming_webhook/getNotas');
   }
 
   getUltimaNota() {
-    return this.http.get('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoapp-rufkc/service/TODOService/incoming_webhook/getUltimaNota');
+    return this.http.get('https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoappv2-zxque/service/TODOService/incoming_webhook/getUltimaNota');
   }
 
   insertNota(notaNumeroOrden, notaTexto) {
-    var url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoapp-rufkc/service/TODOService/incoming_webhook/insertNota';
+    var url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoappv2-zxque/service/TODOService/incoming_webhook/insertNota';
     url = url + '?notaNumeroOrden=' + notaNumeroOrden + '&notaTexto=' + notaTexto;
     return this.http.get(url);
   }
 
   updateNotaNumeroOrden(notaId, notaNumeroOrden) {
-    var url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoapp-rufkc/service/TODOService/incoming_webhook/updateNotaNumeroOrden';
+    var url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoappv2-zxque/service/TODOService/incoming_webhook/updateNotaNumeroOrden';
     url = url + '?notaId=' + notaId + '&notaNumeroOrden=' + notaNumeroOrden;
     return this.http.get(url);
   }
 
   updateNotaTexto(notaId, notaTexto) {
-    var url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoapp-rufkc/service/TODOService/incoming_webhook/updateNotaTexto';
+    var url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoappv2-zxque/service/TODOService/incoming_webhook/updateNotaTexto';
     url = url + '?notaId=' + notaId + '&notaTexto=' + notaTexto;
     return this.http.get(url);
   }
 
   deleteNota(notaId) {
-    var url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoapp-rufkc/service/TODOService/incoming_webhook/deleteNota';
+    var url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/todoappv2-zxque/service/TODOService/incoming_webhook/deleteNota';
     url = url + '?notaId=' + notaId;
     return this.http.get(url);
   }
